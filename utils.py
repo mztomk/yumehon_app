a12a57dad69ef61ff1dc6cae157e201086576dd4
tom <mz.tomk@gmail.com>
2025-09-19 06:43:54 +0900

    utils.py を更新

--- utils.py
+++ utils.py
@@ -11,6 +11,7 @@ from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips
 import os
 import json
 from dotenv import load_dotenv
+import streamlit as st
 
 
 
@@ -143,23 +144,11 @@ def synthesize_tts_google(ssml: str, out_path: str, tts_gender: str = "女のひ
     - tts_gender: "女性" or "男性"
     """
 
-    # .env から読み込んだキーを dict にする
-    load_dotenv()
+    # ✅ Streamlit Cloud の secrets から認証情報を読み込む
+    credentials_info = st.secrets["gcp_service_account"]
 
-    credentials_info = {
-        "type": "service_account",
-        "project_id": os.getenv("GOOGLE_PROJECT_ID"),
-        "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
-        "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace("\\n", "\n"),
-        "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
-        "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL"),
-    }
-
-    client = texttospeech.TextToSpeechClient.from_service_account_info(credentials_info)
+    # ✅ サービスアカウント情報でクライアント作成
+    client = texttospeech.TextToSpeechClient.from_service_account_info(dict(credentials_info))
 
     if tts_gender == "女のひと":
         voice_name = "ja-JP-Wavenet-A"
